0416a38dda775024e950c69c455e9635fcf3363c
Phil Shapiro <philshap@gmail.com>
2021-12-14 11:16:02 -0500

    day 4

---
+++ src/day4.clj
@@ -0,0 +1,31 @@
+(ns day4
+  (:require [clojure.string :as str])
+  (:import (java.security MessageDigest)))
+
+(defn md5 [^String s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        raw (.digest algorithm (.getBytes s))]
+    (format "%032x" (BigInteger. 1 raw))))
+
+(def input "ckczppom")
+
+(defn is-target-md5? [target n]
+  (-> (str input n)
+      md5
+      (str/starts-with? target)))
+
+(defn find-md5-match [target]
+  (->> (range)
+       (drop-while (complement (partial is-target-md5? target)))
+       first))
+
+(defn part1 []
+  (find-md5-match "00000"))
+
+(defn part2 []
+  (find-md5-match "000000"))
+
+(comment
+  (println "part 1: " (part1))
+  (println "part 2: " (part2))
+  )
