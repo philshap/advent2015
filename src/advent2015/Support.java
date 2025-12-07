b9a96a399ded0f14e620b8b0f78e3dc61eb24ea1
Phil Shapiro <philshap@gmail.com>
2025-12-07 15:51:52 -0500

    day 21

--- src/advent2015/Support.java
+++ src/advent2015/Support.java
@@ -2,7 +2,6 @@ package advent2015;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.Spliterators;
